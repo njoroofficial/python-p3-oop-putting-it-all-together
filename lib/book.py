15165fcc93f854bf4efc51219196188388461a63
njoroofficial <mosesn579@gmail.com>
2026-01-07 12:18:44 +0300

    python property decorator

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = "And Then There Were None", page_count = 272):
+        self.title = title
+        self.page_count = page_count
+
+    # get page count
+    @property
+    def page_count(self):
+        return self._page_count
     
+    # set page count
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value , int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
         
+    # turn page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+
